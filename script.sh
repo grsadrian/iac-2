b4d2038c32014f240afbc9dedf245a124ba52a53
Adrian Silva <grsadrian01@outlook.com>
2024-12-06 21:22:10 -0300

    fix(script): corrige erro de copia do site para diretório apache

--- script.sh
+++ script.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
 echo "Atualizando repositórios..."
-sleep 1
+sleep 3
 apt update
 apt upgrade
 echo "Repositórios atualizados !"
 
 echo "Instalando Apache..."
-sleep 1
+sleep 3
 apt install apache2
 echo "Apache instalado !"
 
 echo "Instalando unzip..."
-sleep 1
+sleep 3
 apt install unzip
 echo "Unzip instalado !"
 
 cd /tmp
 echo "Baixando o repositório do professor Denilson Bonatti..."
+sleep 3
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 echo "Descompactando e copiando arquivos para diretório Apache..."
+sleep 3
 unzip main.zip
-cd linux-site-dio
+cd linux-site-dio-main
 cp -r -v * /var/www/html
 
 echo "Script finalizado :)"
-sleep 2
+sleep 4
 
